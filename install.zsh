336d71d10867c7ed73f588a2885055f9feb37151
Kevin Su <ksu006@gmail.com>
2016-09-02 01:50:06 -0700

    Install spf13-vim if necessary

--- install.zsh
+++ install.zsh
@@ -1,3 +1,11 @@
 #!/usr/bin/env zsh
+set -e
+
 command -v stow >/dev/null 2>&1 || { echo "Please install GNU stow." >&2; exit 1;  }
 stow --dir=${${(%):-%N}:A:h} --target=$HOME --restow git idea tmux vim zsh
+
+# Install spf13-vim
+# Note: do this after stow command so that we only install the vim plugins we want.
+if [[ ! -d $HOME/.spf13-vim-3 ]]; then
+    curl https://j.mp/spf13-vim3 -L -o - | sh
+fi
