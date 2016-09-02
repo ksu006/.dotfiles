3426fdc5882ae42053ce39b10b452285b05c927a
Kevin Su <sukevin@google.com>
2016-09-01 19:48:38 -0700

    Use --restow in install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 command -v stow >/dev/null 2>&1 || { echo "Please install GNU stow." >&2; exit 1;  }
 command -v readlink >/dev/null 2>&1 || { echo "Please install readlink." >&2; exit 1;  }
-stow --dir=$(dirname $(readlink -f $0)) --target=$HOME git idea tmux vim zsh
+stow --dir=$(dirname $(readlink -f $0)) --target=$HOME --restow git idea tmux vim zsh
