48c835fccbf815745600ceaba3b65b8a4fb10b9f
Kevin Su <ksu006@gmail.com>
2017-01-26 15:29:28 -0800

    Source shell completions of fzf.

--- zsh/.zplug.packages.zsh
+++ zsh/.zplug.packages.zsh
@@ -1,9 +1,10 @@
 zplug "chriskempson/base16-shell", use:"scripts/base16-ir-black.sh", defer:1
+zplug "junegunn/fzf", use:"shell/*.zsh", on:"junegunn/fzf-bin", defer:1
 zplug "junegunn/fzf-bin", from:gh-r, as:command, rename-to:fzf, defer:1
 zplug "lib/*", from:oh-my-zsh
 zplug "mafredri/zsh-async", on:"sindresorhus/pure", defer:1
 zplug "sindresorhus/pure", defer:1
-zplug 'zplug/zplug', hook-build:'zplug --self-manage'
+zplug "zplug/zplug", hook-build:"zplug --self-manage"
 zplug "zsh-users/zsh-autosuggestions", defer:1
 zplug "zsh-users/zsh-completions", defer:1
 zplug "zsh-users/zsh-syntax-highlighting", defer:2
