1961b66094c93435a9d7fb61625c7f49976a1594
Kevin Su <ksu006@gmail.com>
2017-01-28 13:46:55 -0800

    Pair down oh-my-zsh plugins and simplify plug defer.

--- zsh/.zplug.packages.zsh
+++ zsh/.zplug.packages.zsh
@@ -1,14 +1,15 @@
-zplug "chriskempson/base16-shell", use:"scripts/base16-ir-black.sh", defer:1
+zplug "chriskempson/base16-shell", use:"scripts/base16-ir-black.sh"
 zplug "junegunn/fzf", use:"shell/*.zsh", on:"junegunn/fzf-bin", \
-    hook-load:"export FZF_DEFAULT_OPTS='--exact'", defer:1
+    hook-load:"export FZF_DEFAULT_OPTS='--exact'"
 zplug "junegunn/fzf", as:command, use:"bin/fzf-tmux", on:"junegunn/fzf-bin", \
-    hook-load:"export FZF_TMUX=1", defer:1
-zplug "junegunn/fzf-bin", from:gh-r, as:command, rename-to:fzf, defer:1
-zplug "lib/*", from:oh-my-zsh
-zplug "mafredri/zsh-async", on:"sindresorhus/pure", defer:1
-zplug "sindresorhus/pure", defer:1
+    hook-load:"export FZF_TMUX=1"
+zplug "junegunn/fzf-bin", from:gh-r, as:command, rename-to:fzf
+zplug "lib/directories", from:oh-my-zsh
+zplug "lib/history", from:oh-my-zsh
+zplug "mafredri/zsh-async", on:"sindresorhus/pure"
+zplug "sindresorhus/pure"
 zplug "zplug/zplug", hook-build:"zplug --self-manage"
-zplug "zsh-users/zsh-completions", defer:1
+zplug "zsh-users/zsh-completions"
 zplug "zsh-users/zsh-syntax-highlighting", defer:2
 
 # Install plugins if there are plugins that have not been installed
