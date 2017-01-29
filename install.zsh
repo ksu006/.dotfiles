70a4376a5af81aee3b0e6598829e837ebd64bc74
Kevin Su <ksu006@gmail.com>
2017-01-29 04:11:13 -0800

    Space

--- install.zsh
+++ install.zsh
@@ -1,7 +1,7 @@
 #!/usr/bin/env zsh
 set -e
 
-command -v stow >/dev/null 2>&1 || { echo "Please install GNU stow." >&2; exit 1;  }
+command -v stow > /dev/null 2>&1 || { echo "Please install GNU stow." >&2; exit 1;  }
 stow --dir=${${(%):-%N}:A:h} --target=$HOME --restow git idea tmux vim zsh
 
 # Install spf13-vim
