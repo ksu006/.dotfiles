0d1d9d5b66acbb4c006a2b1ebc5bf29b316e4600
Kevin Su <ksu006@gmail.com>
2017-01-30 12:00:27 -0800

    Reorder .zplug.packages.zsh

--- zsh/.zplug.packages.zsh
+++ zsh/.zplug.packages.zsh
@@ -1,8 +1,28 @@
 # zplug packages
 # See https://github.com/zplug/zplug
 
+# zplug - Zsh plugin manager
+# Manage zplug itself like other packages.
+zplug "zplug/zplug", hook-build:"zplug --self-manage"
+
+# oh-my-zsh
+zplug "lib/directories", from:oh-my-zsh
+zplug "lib/history", from:oh-my-zsh
+zplug "lib/key-bindings", from:oh-my-zsh
+zplug "plugins/zsh_reload", from:oh-my-zsh
+
 # base16-shell - Syntax highlighting for hackers
 zplug "chriskempson/base16-shell", use:"scripts/base16-ir-black.sh"
+
+# pure - Pretty, minimal and fast zsh prompt
+zplug "sindresorhus/pure"
+zplug "mafredri/zsh-async", on:"sindresorhus/pure"
+
+# zsh-completions - Additional completion definitions for zsh
+zplug "zsh-users/zsh-completions"
+
+# zsh-syntax-highlighting - Fish shell like syntax highlighting for zsh
+zplug "zsh-users/zsh-syntax-highlighting", defer:2
 
 # fzf - Command-line fuzzy finder
 # Command-line binary.
@@ -13,26 +33,6 @@ zplug "junegunn/fzf", use:"shell/*.zsh", on:"junegunn/fzf-bin", \
 # Open fzf in tmux pane if running in tmux.
 zplug "junegunn/fzf", as:command, use:"bin/fzf-tmux", on:"junegunn/fzf-bin", \
     hook-load:"export FZF_TMUX=1"
-
-# oh-my-zsh
-zplug "lib/directories", from:oh-my-zsh
-zplug "lib/history", from:oh-my-zsh
-zplug "lib/key-bindings", from:oh-my-zsh
-zplug "plugins/zsh_reload", from:oh-my-zsh
-
-# pure - Pretty, minimal and fast zsh prompt
-zplug "sindresorhus/pure"
-zplug "mafredri/zsh-async", on:"sindresorhus/pure"
-
-# zplug - Zsh plugin manager
-# Manage zplug itself like other packages.
-zplug "zplug/zplug", hook-build:"zplug --self-manage"
-
-# zsh-completions - Additional completion definitions for zsh
-zplug "zsh-users/zsh-completions"
-
-# zsh-syntax-highlighting - Fish shell like syntax highlighting for zsh
-zplug "zsh-users/zsh-syntax-highlighting", defer:2
 
 # Install plugins if there are plugins that have not been installed
 if ! zplug check --verbose; then
