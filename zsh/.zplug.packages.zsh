4b2dfa94cdd909dec9ba1fed0a4844ef8a82c150
Kevin Su <ksu006@gmail.com>
2017-01-29 16:19:19 -0800

    Add zsh_reload

--- zsh/.zplug.packages.zsh
+++ zsh/.zplug.packages.zsh
@@ -18,6 +18,7 @@ zplug "junegunn/fzf", as:command, use:"bin/fzf-tmux", on:"junegunn/fzf-bin", \
 zplug "lib/directories", from:oh-my-zsh
 zplug "lib/history", from:oh-my-zsh
 zplug "lib/key-bindings", from:oh-my-zsh
+zplug "plugins/zsh_reload", from:oh-my-zsh
 
 # pure - Pretty, minimal and fast zsh prompt
 zplug "sindresorhus/pure"
