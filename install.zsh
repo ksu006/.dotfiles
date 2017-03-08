784df6946c791fb2dc88e5f0fab80abe8c957f52
Kevin Su <ksu006@gmail.com>
2017-03-08 13:05:21 -0800

    Fix install

--- install.zsh
+++ install.zsh
@@ -1,5 +1,8 @@
 #!/usr/bin/env zsh
 set -e
 
-command -v stow > /dev/null 2>&1 || { echo "Please install GNU stow." >&2; exit 1;  }
-stow --dir=${${(%):-%N}:A:h} --target=$HOME --restow git idea tmux vim zsh
+cd ${0:a:h}
+DIR=$(pwd)
+for rc in *rc gitconfig tmux.conf zplug.packages.zsh; do
+  ln -sfv $DIR/$rc ~/.$rc
+done
