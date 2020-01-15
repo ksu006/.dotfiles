9e5eb5dafea0d31c850452e87f4fa87fa9c1cda9
Kevin Su <972071+ksu006@users.noreply.github.com>
2020-01-14 23:43:02 -0800

    Clone kdrag0n/base16-kitty in install.zsh.

--- install.zsh
+++ install.zsh
@@ -10,3 +10,4 @@ for rc in *rc gitconfig tmux.conf; do
 done
 [ -e ~/.config/kitty/kitty.conf ] && mv -v ~/.config/kitty/kitty.conf bak/kitty.conf
 ln -sfv $DIR/kitty.conf ~/.config/kitty/kitty.conf
+[ ! -d ~/.config/base16-kitty ] && git clone https://github.com/kdrag0n/base16-kitty.git ~/.config/base16-kitty
